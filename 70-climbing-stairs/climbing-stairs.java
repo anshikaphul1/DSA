76150f06a42744de1160b9bab2886edb32c165e8
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2025-07-10 23:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (65.72%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int x=1;
+        int y=2;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        for(int i=3;i<=n;i++){
+            int z=x+y;
+            x=y;
+            y=z;
+        }
+        return y;
+    }
+}
