a14de66d2e05445db91d02e8bc1a8783b2532139
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2025-09-03 14:43:17 +0530

    Time: 29 ms (87.01%) | Memory: 56.8 MB (43.57%) - LeetSync

---
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        // int max=nums[nums.length-1];
+        int l=0,r=0;
+        int res=0;
+        long total=0;
+        while(r<nums.length){
+            total+=nums[r];
+            while((long)nums[r]*(r-l+1)>total+k){//invalid window use kar rhe hai mtlb if 3>5 to hi chalega or hum window ko shrink karenge
+            total-=nums[l];
+            l+=1;
+        }
+        res=Math.max(res,r-l+1);
+            r+=1;
+        }
+        return res;
+    }
+}
