0bb428209d806b92a73cc713e125b812e7c08b34
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2025-09-14 00:50:39 +0530

    Time: 3 ms (60.85%) | Memory: 42.8 MB (31.34%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -3,21 +3,41 @@ class Solution {
         return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
     }
     public int maxFreqSum(String s) {
-        int[] consonent=new int[26];
-        int[] vowel=new int[26];
-        int maxconsonent=0;
+        // int[] consonent=new int[26];
+        // int[] vowel=new int[26];
+        // int maxconsonent=0;
+        // int maxvowel=0;
+        // for(char c:s.toCharArray()){
+        //     if(isVowel(c)){
+        //         vowel[c-'a']++;
+        //     }
+        //     else{
+        //         consonent[c-'a']++;
+        //     }
+        // }
+        // for(int i=0;i<26;i++){
+        //     maxvowel=Math.max(maxvowel,vowel[i]);
+        //     maxconsonent=Math.max(maxconsonent,consonent[i]);
+        // }
+        // return maxvowel+maxconsonent;
+
         int maxvowel=0;
+        int maxconsonent=0;
+        HashMap<Character,Integer> vowel=new HashMap<>();
+        HashMap<Character,Integer> consonent=new HashMap<>();
         for(char c:s.toCharArray()){
             if(isVowel(c)){
-                vowel[c-'a']++;
+                vowel.put(c,vowel.getOrDefault(c,0)+1);
             }
             else{
-                consonent[c-'a']++;
+                consonent.put(c,consonent.getOrDefault(c,0)+1);
             }
         }
-        for(int i=0;i<26;i++){
-            maxvowel=Math.max(maxvowel,vowel[i]);
-            maxconsonent=Math.max(maxconsonent,consonent[i]);
+        for(int freq:vowel.values()){
+            maxvowel=Math.max(maxvowel,freq);
+        }
+        for(int freq:consonent.values()){
+            maxconsonent=Math.max(maxconsonent,freq);
         }
         return maxvowel+maxconsonent;
     }
