568fb2446f9f80c7d3affb0c2d08ee6af5e290c9
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2025-09-17 17:16:41 +0530

    Time: 1 ms (99.72%) | Memory: 45.8 MB (66.65%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -6,7 +6,7 @@ class Solution {
             xor^=ele;
         }
             int pos=0;
-            // 
+            // xor ko right shift 
             for(int i=0;i<32;i++){
                 if(((xor>>i) & 1) ==1){
                     pos=i;
