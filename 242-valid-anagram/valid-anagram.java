d75bc76ee3131921fa14e81b879c15b7b899ca6e
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2025-10-22 16:39:33 +0530

    Time: 3 ms (90.30%) | Memory: 44.7 MB (43.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,18 +3,24 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        Map<Character,Integer> count=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            count.put(ch,count.getOrDefault(ch,0) +1);
-        }
-        for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            if(!count.containsKey(ch) || count.get(ch)==0){
-                return false;
-            }
-            count.put(ch,count.get(ch)-1);
-        }
-        return true;
+        // Map<Character,Integer> count=new HashMap<>();
+        // for(int i=0;i<s.length();i++){
+        //     char ch = s.charAt(i);
+        //     count.put(ch,count.getOrDefault(ch,0) +1);
+        // }
+        // for(int i=0;i<t.length();i++){
+        //     char ch=t.charAt(i);
+        //     if(!count.containsKey(ch) || count.get(ch)==0){
+        //         return false;
+        //     }
+        //     count.put(ch,count.get(ch)-1);
+        // }
+        // return true;
+
+        char[] ch=s.toCharArray();
+        char[] ch1=t.toCharArray();
+        Arrays.sort(ch);
+        Arrays.sort(ch1);
+        return Arrays.equals(ch,ch1);
     }
 }
