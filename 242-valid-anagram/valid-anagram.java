3c59e0e7e9e8f424273039a30987c7829c3c255a
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2025-10-22 16:36:19 +0530

    Time: 16 ms (21.13%) | Memory: 44.6 MB (43.17%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        Map<Character,Integer> count=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            count.put(ch,count.getOrDefault(ch,0) +1);
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            if(!count.containsKey(ch) || count.get(ch)==0){
+                return false;
+            }
+            count.put(ch,count.get(ch)-1);
+        }
+        return true;
+    }
+}
