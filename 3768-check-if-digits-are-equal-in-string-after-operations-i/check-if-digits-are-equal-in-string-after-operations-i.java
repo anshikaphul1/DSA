4eafee2650afc823320fdae026a340fb5b6f2e2e
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2025-10-24 00:59:51 +0530

    Time: 8 ms (72.03%) | Memory: 45 MB (42.12%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.java
@@ -1,16 +1,28 @@
 class Solution {
     public boolean hasSameDigits(String s) {
-        int n=s.length();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=s.charAt(i)-'0';
-        }
-        while(n>2){
-            for(int i=0;i<n-1;i++){
-                arr[i]=(arr[i]+arr[i+1])%10;
+        // int n=s.length();
+        // int[] arr=new int[n];
+        // for(int i=0;i<n;i++){
+        //     arr[i]=s.charAt(i)-'0';
+        // }
+        // while(n>2){
+        //     for(int i=0;i<n-1;i++){
+        //         arr[i]=(arr[i]+arr[i+1])%10;
+        //     }
+        //     n--;
+        // }
+        // return arr[0]==arr[1];
+
+
+        while(s.length()>2){
+            StringBuilder string=new StringBuilder();
+            for(int i=0;i<s.length()-1;i++){
+                int a=s.charAt(i)-'0';
+                int b=s.charAt(i+1)-'0';
+                string.append((a+b)%10);
             }
-            n--;
+            s=string.toString();
         }
-        return arr[0]==arr[1];
+        return s.charAt(0)==s.charAt(1);
     }
 }
