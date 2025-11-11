33a14716d4a9c0f587e010eb3e2acace1d236eae
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2025-11-12 00:51:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (6.92%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,25 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        // int k=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]!=val){
-        //         nums[k]=nums[i];
-        //         k++;
-        //     }
-        // }
-        // return k; //new length hai array ki ye without val
-
-
-        // ArrayList se: chatgpt se dekha hai ye
-
-        List<Integer> list=new ArrayList<>();
-        for(int num:nums){
-            list.add(num);
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
         }
-        list.removeIf(n->n==val);
-        for(int i=0;i<list.size();i++){
-            nums[i]=list.get(i);
-        }
-        return list.size();
+        return k;
     }
 }
