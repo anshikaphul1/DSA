bb36e240dec14c7a85b8d78618cdc2651f6a010e
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2026-02-20 02:10:57 +0530

    Time: 2 ms (94.36%) | Memory: 64.8 MB (89.58%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -13,28 +13,45 @@ class Solution {
         // }
         // return res;
 
+        // int n=nums.length;
+        // int[] res=new int[n];
+        // int prod=1;
+        // int zero=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==0){
+        //        zero++;
+        //     }
+        //     else{
+        //         prod*=nums[i];
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(zero>1){
+        //         res[i]=0;
+        //     }
+        //     else if(zero==1){
+        //        res[i]=(nums[i]==0)?prod:0;
+        //     }
+        //     else{
+        //          res[i]=prod/nums[i];
+        //     }
+        // }
+        // return res;
+
         int n=nums.length;
         int[] res=new int[n];
-        int prod=1;
-        int zero=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-               zero++;
-            }
-            else{
-                prod*=nums[i];
-            }
+        int[] pref=new int[n];
+        int[] suff=new int[n];
+        pref[0]=1;
+        suff[n-1]=1;
+        for(int i=1;i<n;i++){
+            pref[i]=nums[i-1]*pref[i-1];
+        }
+        for(int i=n-2;i>=0;i--){
+            suff[i]=nums[i+1]*suff[i+1];
         }
         for(int i=0;i<n;i++){
-            if(zero>1){
-                res[i]=0;
-            }
-            else if(zero==1){
-               res[i]=(nums[i]==0)?prod:0;
-            }
-            else{
-                 res[i]=prod/nums[i];
-            }
+            res[i]=pref[i]*suff[i];
         }
         return res;
     }
