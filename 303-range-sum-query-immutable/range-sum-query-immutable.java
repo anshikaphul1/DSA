a575f7b092625e0d41a2a4f59fa878ca450ea513
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2026-02-20 03:35:32 +0530

    Time: 7 ms (99.66%) | Memory: 48 MB (11.88%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,15 +1,29 @@
 class NumArray {
-    public int[]nums;
-    public NumArray(int[] nums) {
-        this.nums=nums;
-    }
+    // public int[]nums;
+    // public NumArray(int[] nums) {
+    //     this.nums=nums;
+    // }
     
-    public int sumRange(int left, int right) {
-        int sum=0;
-        for(int i=left;i<=right;i++){
-            sum+=nums[i];
+    // public int sumRange(int left, int right) {
+    //     int sum=0;
+    //     for(int i=left;i<=right;i++){
+    //         sum+=nums[i];
+    //     }
+    //     return sum;
+    // }
+
+    public int[] prefixsum;
+    public NumArray(int[] nums){
+        prefixsum=new int[nums.length];
+        prefixsum[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            prefixsum[i]=nums[i]+prefixsum[i-1];
         }
-        return sum;
+    }
+
+    public int sumRange(int left,int right){
+        if(left==0) return prefixsum[right];
+        else return prefixsum[right]-prefixsum[left-1];
     }
 }
 
