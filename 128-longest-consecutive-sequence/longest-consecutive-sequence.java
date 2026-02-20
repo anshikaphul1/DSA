3acd939a0eccc91b7a77a34ed92dc24d7569335f
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2026-02-20 22:07:35 +0530

    Time: 23 ms (83.73%) | Memory: 77.4 MB (96.90%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -14,19 +14,38 @@ class Solution {
         // }
         // return Math.max(longest,count);
 
-        HashSet<Integer>set=new HashSet<>();
+        // HashSet<Integer>set=new HashSet<>();
+        // Arrays.sort(nums);
+        // if(nums.length==0) return 0;
+        // set.add(nums[0]);
+        // int count=1;
+        // int longest=1;
+        // for(int i=1;i<nums.length;i++){
+        //     if(set.contains(nums[i])) continue;
+        //     set.add(nums[i]);
+        //     if(nums[i]==nums[i-1]) continue;
+        //     else if(nums[i]==nums[i-1]+1) count++;
+        //     else count=1;
+        //     longest=Math.max(longest,count);
+        // }
+        // return longest;
+    if(nums.length==0) return 0;
         Arrays.sort(nums);
-        if(nums.length==0) return 0;
-        set.add(nums[0]);
-        int count=1;
-        int longest=1;
-        for(int i=1;i<nums.length;i++){
-            if(set.contains(nums[i])) continue;
-            set.add(nums[i]);
-            if(nums[i]==nums[i-1]) continue;
-            else if(nums[i]==nums[i-1]+1) count++;
-            else count=1;
-            longest=Math.max(longest,count);
+        int longest=1,currcnt=0,lastsmall=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]-1==lastsmall){
+                currcnt++;
+                lastsmall=nums[i];
+            }
+            else if(nums[i]==lastsmall){
+                continue;
+            }
+            else if(nums[i]!=lastsmall){
+                // largest=Math.max(largest,currcnt);
+                currcnt=1;
+                lastsmall=nums[i];
+            }
+            longest=Math.max(longest,currcnt);
         }
         return longest;
     }
