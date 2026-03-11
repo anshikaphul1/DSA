d40f2744968117c51fec991cdb5fe9bb3ef770ba
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2026-03-11 15:53:11 +0530

    Time: 0 ms (100.00%) | Memory: 116.3 MB (38.65%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -2,19 +2,49 @@ class Solution {
     public int[] findPeakGrid(int[][] mat) {
         int m=mat.length;
         int n=mat[0].length;
-        int max=Integer.MIN_VALUE;
-        int[] res=new int[2];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]>max){
-                    max=mat[i][j];
-                    res[0]=i;
-                    res[1]=j;
-                }
-                else
-                continue;
+        // int max=Integer.MIN_VALUE;
+        // int[] res=new int[2];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(mat[i][j]>max){
+        //             max=mat[i][j];
+        //             res[0]=i;
+        //             res[1]=j;
+        //         }
+        //         else
+        //         continue;
+        //     }
+        // }
+        // return res;
+
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int row=maxele(mat,mid);
+            int left=mid-1>=0?mat[row][mid-1]:-1;
+            int right=mid+1<n?mat[row][mid+1]:-1;
+            if(mat[row][mid]>left && mat[row][mid]>right){
+                return new int[]{row,mid};
             }
+            else if(mat[row][mid]<left){
+                high=mid-1;
+            }
+            else
+            low=mid+1;
         }
-        return res;
+        return new int[]{-1,-1};
     }
+     public int maxele(int[][]mat,int col){
+            int n=mat.length;
+            int index=-1;
+            int max=Integer.MIN_VALUE;
+            for(int i=0;i<n;i++){
+                if(mat[i][col]>max){
+                    max=mat[i][col];
+                    index=i;
+                }
+            }
+            return index;
+        }
 }
