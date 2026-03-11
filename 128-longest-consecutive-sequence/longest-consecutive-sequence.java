ea36388531c35fd7257601b40cc64f48e41ce765
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2026-03-11 17:39:36 +0530

    Time: 48 ms (10.37%) | Memory: 101.2 MB (5.00%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,74 +1,28 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        // Arrays.sort(nums);
-        // if(nums.length==0) return 0;
-        // int count=1;
-        // int longest=1;
-        // for(int i=1;i<nums.length;i++){
-        //     if(nums[i]==nums[i-1]) continue;
-        //     else if(nums[i]==nums[i-1]+1) count++;
-        //     else {
-        //         longest=Math.max(longest,count);
-        //         count=1;//reset krna hota hai pattern
-        //     }
-        // }
-        // return Math.max(longest,count);
-
-        // HashSet<Integer>set=new HashSet<>();
-        // Arrays.sort(nums);
-        // if(nums.length==0) return 0;
-        // set.add(nums[0]);
-        // int count=1;
-        // int longest=1;
-        // for(int i=1;i<nums.length;i++){
-        //     if(set.contains(nums[i])) continue;
-        //     set.add(nums[i]);
-        //     if(nums[i]==nums[i-1]) continue;
-        //     else if(nums[i]==nums[i-1]+1) count++;
-        //     else count=1;
-        //     longest=Math.max(longest,count);
-        // }
-        // return longest;
-    // if(nums.length==0) return 0;
-    //     Arrays.sort(nums);
-    //     int longest=1,currcnt=0,lastsmall=Integer.MIN_VALUE;
-    //     for(int i=0;i<nums.length;i++){
-    //         if(nums[i]-1==lastsmall){
-    //             currcnt++;
-    //             lastsmall=nums[i];
-    //         }
-    //         else if(nums[i]==lastsmall){
-    //             continue;
-    //         }
-    //         else if(nums[i]!=lastsmall){
-    //             // largest=Math.max(largest,currcnt);
-    //             currcnt=1;
-    //             lastsmall=nums[i];
-    //         }
-    //         longest=Math.max(longest,currcnt);
-    //     }
-    //     return longest;
-
-
-    if(nums.length==0) return 0;
-    int longest=1;
-    // int cnt=0;
-    HashSet<Integer>set=new HashSet<>();
-    for(int num:nums){
-        set.add(num);
-    }
-    for(int n:set){
-        if(!set.contains(n-1)){
-            int cnt=1;
-            int x=n;
-        
-        while(set.contains(x+1)){
-            cnt++;
-            x++;
+        HashMap<Integer,Boolean> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i]-1)){
+                map.put(nums[i],false);
+            }
+            else{
+                map.put(nums[i],true);
+            }
+            if(map.containsKey(nums[i]+1)){
+                map.put(nums[i]+1,false);
+            }
         }
-        longest=Math.max(longest,cnt);
+        int ans=0;
+        for(int key:map.keySet()){
+            if(map.get(key)){
+                int count=0;
+                while(map.containsKey(key)){
+                    count++;
+                    key++;
+                }
+                ans=Math.max(ans,count);
+            }
         }
-    }
-    return longest;
+        return ans;
     }
 }
