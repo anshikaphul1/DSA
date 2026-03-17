7ecd4f20f3defd5f02baf77ce9b90336281a4f4b
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2026-03-17 17:40:22 +0530

    Time: 7 ms (17.99%) | Memory: 45.1 MB (36.53%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -17,27 +17,53 @@ class Solution {
         // }
         // return ans;
 
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+        // List<Integer>result=new ArrayList<>();
+        // int i=0,j=0;
+        // while(i<nums1.length && j<nums2.length){
+        //     if(nums1[i]==nums2[j]){
+        //         if(result.isEmpty() || result.get(result.size()-1)!=nums1[i]){
+        //             result.add(nums1[i]);
+        //         }
+        //         i++;
+        //     j++;
+        //     }
+        //     else if(nums1[i]<nums2[j]){
+        //         i++;
+        //     } 
+        //     else j++;
+        // }
+        // int[]ans=new int[result.size()];
+        // for(int k=0;k<ans.length;k++){
+        //     ans[k]=result.get(k);
+        // }
+        // return ans;
+
         Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        List<Integer>result=new ArrayList<>();
-        int i=0,j=0;
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i]==nums2[j]){
-                if(result.isEmpty() || result.get(result.size()-1)!=nums1[i]){
-                    result.add(nums1[i]);
-                }
-                i++;
-            j++;
+        HashSet<Integer>set=new HashSet<>();
+        for(int num:nums2){
+            if(binary(nums1,num)){
+                set.add(num);
             }
-            else if(nums1[i]<nums2[j]){
-                i++;
-            } 
-            else j++;
         }
-        int[]ans=new int[result.size()];
-        for(int k=0;k<ans.length;k++){
-            ans[k]=result.get(k);
+        int[] ans=new int[set.size()];
+        int i=0;
+        for(int num:set){
+            ans[i++]=num;
         }
         return ans;
+    }
+    public boolean binary(int[]arr,int target){
+        int l=0;
+        int r=arr.length-1;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(arr[mid]==target) return true;
+            else if(arr[mid]<target) l=mid+1;
+            else r=mid-1;
+        }
+        return false;
     }
 }
