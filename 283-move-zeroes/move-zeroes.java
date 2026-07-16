3444ee2f09855ea030d374c3c591bf2b6a711bbf
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2026-07-16 18:36:08 +0530

    Time: 2 ms (92.19%) | Memory: 47.9 MB (38.39%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,26 +1,26 @@
 class Solution {
-    // public void moveZeroes(int[] nums) {
-    //     int l=0;//position jaha next non zero element will be moved
-    //     for(int r=0;r<nums.length;r++){
-    //         if(nums[r]!=0){
-    //             int temp=nums[r];
-    //             nums[r]=nums[l];
-    //             nums[l]=temp;
-    //             l++;
-    //         }
-    //     }
-    // }
-    public void moveZeroes(int[]nums){
-        int k=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                nums[k]=nums[i];
-                k++;
+    public void moveZeroes(int[] nums) {
+        int l=0;//position jaha next non zero element will be moved
+        for(int r=0;r<nums.length;r++){
+            if(nums[r]!=0){
+                int temp=nums[r];
+                nums[r]=nums[l];
+                nums[l]=temp;
+                l++;
             }
         }
-        while(k<nums.length){
-            nums[k]=0;
-            k++;
-        }
     }
+    // public void moveZeroes(int[]nums){
+    //     int k=0;
+    //     for(int i=0;i<nums.length;i++){
+    //         if(nums[i]!=0){
+    //             nums[k]=nums[i];
+    //             k++;
+    //         }
+    //     }
+    //     while(k<nums.length){
+    //         nums[k]=0;
+    //         k++;
+    //     }
+    // }
 }
