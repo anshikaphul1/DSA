f6663959b9a8949e0e0d3dce7385ecff7a5bf3de
Anshika <157566134+anshikaphul1@users.noreply.github.com>
2026-07-16 18:32:54 +0530

    Time: 2 ms (92.19%) | Memory: 47.9 MB (38.39%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,5 +1,16 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
+    // public void moveZeroes(int[] nums) {
+    //     int l=0;//position jaha next non zero element will be moved
+    //     for(int r=0;r<nums.length;r++){
+    //         if(nums[r]!=0){
+    //             int temp=nums[r];
+    //             nums[r]=nums[l];
+    //             nums[l]=temp;
+    //             l++;
+    //         }
+    //     }
+    // }
+    public void moveZeroes(int[]nums){
         int k=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
@@ -7,7 +18,6 @@ class Solution {
                 k++;
             }
         }
-        // System.out.println(k);
         while(k<nums.length){
             nums[k]=0;
             k++;
